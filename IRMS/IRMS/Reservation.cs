f91544ea2c69d8fd4e25c4252d0ea78e37b7b2cc
JoelByers <joel.byers@outlook.com>
2022-03-05 11:12:50 -0600

    Added Reservation, Reservation Controller

---
+++ IRMS/IRMS/Reservation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRMS
+{
+    class Reservation
+    {
+        string name;
+        string phoneNumber;
+        DateTime expectedTime;
+        DateTime lateTime;
+        int partySize;
+        bool isSeated;
+
+        public Reservation(string name, string phoneNumber, DateTime expectedTime, DateTime lateTime, int partySize)
+        {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+            this.expectedTime = expectedTime;
+            this.lateTime = lateTime;
+            this.partySize = partySize;
+            isSeated = false;
+        }
+
+        public void setName(string newName)
+        {
+            name = newName;
+        }
+
+        public void setPhoneNumber(string newPhoneNumber)
+        {
+            phoneNumber = newPhoneNumber;
+        }
+
+        public void setExpectedTime(DateTime newExpectedTime)
+        {
+            expectedTime = newExpectedTime;
+        }
+
+        public void setPartySize(int newPartySize)
+        {
+            partySize = newPartySize;
+        }
+
+        public void setIsSeated(bool newIsSeated)
+        {
+            isSeated = newIsSeated;
+        }
+    }
+}
---
+++ IRMS/IRMS/ReservationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRMS
+{
+    class ReservationController
+    {
+        private List<Reservation> reservationTable = new List<Reservation>();
+
+        public Reservation getReservationAt(int index)
+        {
+            return reservationTable[index];
+        }
+
+        public void createReservation(string name, string phoneNumber, DateTime expectedTime, int partySize)
+        {
+            // TODO late time is currently a placeholder with DateTime.Now
+            reservationTable.Add(new Reservation(name, phoneNumber, expectedTime, DateTime.Now, partySize));
+        }
+
+        public void editReservation(int reservationIndex, string newName, string newPhoneNumber, DateTime newExpectedTime)
+        {
+            Reservation thisReservation = reservationTable[reservationIndex];
+            thisReservation.setName(newName);
+            thisReservation.setPhoneNumber(newPhoneNumber);
+            thisReservation.setExpectedTime(newExpectedTime);
+
+            // TODO figure out late time
+        }
+
+        public void seatReservation(int reservationIndex, bool newIsSeated)
+        {
+            reservationTable[reservationIndex].setIsSeated(newIsSeated);
+        }
+    }
+}
