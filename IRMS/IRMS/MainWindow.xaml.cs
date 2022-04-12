e46df1468e3d5282a80ffcded077c7a23aee960b
JoelByers <joel.byers@outlook.com>
2022-04-12 10:41:00 -0500

    Reservation Controller Integration with GUI

--- IRMS/IRMS/MainWindow.xaml.cs
+++ IRMS/IRMS/MainWindow.xaml.cs
@@ -20,11 +20,33 @@ namespace IRMS
     /// </summary>
     public partial class MainWindow : Window
     {
+        struct ReservationTimeSlot
+        {
+            public Button button;
+            public int numSeats;
+        };
+
         public MainWindow()
         {
             InitializeComponent();
 
+            ReservationController reservationController = new ReservationController();
+            ReservationTimeSlot[] reservationTimeSlots = new ReservationTimeSlot[reservationController.getNumTimeSlots()];
             Reservation res = new Reservation("Bob", "123-456-7890","10:00", "10:15", 4);
+
+            reservationTimeSlots[0].button = RsvtnBtnTime1;
+            reservationTimeSlots[1].button = RsvtnBtnTime2;
+            reservationTimeSlots[2].button = RsvtnBtnTime3;
+            reservationTimeSlots[3].button = RsvtnBtnTime4;
+            reservationTimeSlots[4].button = RsvtnBtnTime5;
+            reservationTimeSlots[5].button = RsvtnBtnTime6;
+            reservationTimeSlots[6].button = RsvtnBtnTime7;
+
+            for(int i = 0; i < reservationTimeSlots.Length; i++)
+            {
+                reservationTimeSlots[i].numSeats = reservationController.getNumSeats();
+                reservationTimeSlots[i].button.Content = reservationTimeSlots[i].numSeats;
+            }
 
             ReservationGrid.Items.Add(res);
         }
@@ -48,7 +70,7 @@ namespace IRMS
 
         }
 
-        public void Menu1Click(object sender, RoutedEventArgs e)
+        public void MenuRsvtnBtn(object sender, RoutedEventArgs e)
         {
             //Set Menu1Tab to Visible or Collased
 
--- IRMS/IRMS/ReservationController.cs
+++ IRMS/IRMS/ReservationController.cs
@@ -6,22 +6,35 @@ namespace IRMS
 {
     class ReservationController
     {
-        private List<Reservation> reservationTable = new List<Reservation>();
+        private const int closingTime = 12;
+        private const int numTimeSlots = 7;
+        private const int numSeats = 50;
+        private List<Reservation>[] reservationTable = new List<Reservation>[numTimeSlots];
 
-        public Reservation getReservationAt(int index)
+        public ReservationController()
         {
-            return reservationTable[index];
+            for(int i = 0; i < reservationTable.Length; i++)
+            {
+                reservationTable[i] = new List<Reservation>();
+            }
+        }
+
+        public Reservation getReservationAt(int timeIndex, int reservationIndex)
+        {
+            return reservationTable[timeIndex][reservationIndex];
         }
 
         public void createReservation(string name, string phoneNumber, string expectedTime, int partySize)
         {
             // TODO late time is currently a placeholder with DateTime.Now
-            reservationTable.Add(new Reservation(name, phoneNumber, expectedTime, DateTime.Now.ToString(), partySize));
+            int hour = Int32.Parse(expectedTime.Substring(0, 2));
+
+            reservationTable[closingTime-hour].Add(new Reservation(name, phoneNumber, expectedTime, DateTime.Now.ToString(), partySize));
         }
 
-        public void editReservation(int reservationIndex, string newName, string newPhoneNumber, string newExpectedTime)
+        public void editReservation(int timeIndex,int reservationIndex, string newName, string newPhoneNumber, string newExpectedTime)
         {
-            Reservation thisReservation = reservationTable[reservationIndex];
+            Reservation thisReservation = reservationTable[timeIndex][reservationIndex];
             thisReservation.name = newName;
             thisReservation.phoneNumber = newPhoneNumber;
             thisReservation.expectedTime = newExpectedTime;
@@ -29,9 +42,19 @@ namespace IRMS
             // TODO figure out late time
         }
 
-        public void seatReservation(int reservationIndex, bool newIsSeated)
+        public void seatReservation(int timeIndex, int reservationIndex, bool newIsSeated)
         {
-            reservationTable[reservationIndex].isSeated = newIsSeated;
+            reservationTable[timeIndex][reservationIndex].isSeated = newIsSeated;
+        }
+
+        public int getNumSeats()
+        {
+            return numSeats;
+        }
+
+        public int getNumTimeSlots()
+        {
+            return numTimeSlots;
         }
     }
 }
