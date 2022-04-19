1f5140857e93d6ae9aa10b001f621daa736cd431
JoelByers <joel.byers@outlook.com>
2022-04-19 17:17:33 -0500

    Added Error Handling for Reservation

--- IRMS/IRMS/MainWindow.xaml.cs
+++ IRMS/IRMS/MainWindow.xaml.cs
@@ -89,51 +89,200 @@ namespace IRMS
             string name = RsvtnTextBoxNewName.Text;
             string phoneNumber = RsvtnTextBoxNewNumber.Text;
             string expectedTime = RsvtnTextBoxNewTime.Text;
-            int partySize = Int32.Parse(RsvtnTextBoxNewSize.Text.Trim());
+            string partySizeString = RsvtnTextBoxNewSize.Text;
+            int partySize = -1;
 
-            Reservation newReservation = new Reservation(name, phoneNumber, expectedTime, "10:00",partySize);
-            reservationController.createReservation(newReservation);
-            reservationTimeSlots[reservationController.timeToIndex(expectedTime)].numSeats--;
-            reservationTimeSlots[reservationController.timeToIndex(expectedTime)].button.Content = reservationTimeSlots[reservationController.timeToIndex(expectedTime)].numSeats;
+            if(name == "")
+            {
+                RsvtnTextBlockInvalidMessage.Visibility = System.Windows.Visibility.Visible;
+                RsvtnTextBlockInvalidMessage.Text = "Invalid Name: Name must be at least one character";
+            }
+            else if(!phoneNumberIsValid(phoneNumber))
+            {
+                RsvtnTextBlockInvalidMessage.Visibility = System.Windows.Visibility.Visible;
+                RsvtnTextBlockInvalidMessage.Text = "Invalid Phone: Must be in format xxx-xxx-xxxx";
+            }
+            else if(!partySizeStringIsValid(partySizeString))
+            {
+                RsvtnTextBlockInvalidMessage.Visibility = System.Windows.Visibility.Visible;
+                RsvtnTextBlockInvalidMessage.Text = "Invalid Party Size: Must be a number";
+
+            }
+            else if(!timeStringIsValid(expectedTime))
+            {
+                RsvtnTextBlockInvalidMessage.Visibility = System.Windows.Visibility.Visible;
+                RsvtnTextBlockInvalidMessage.Text = "Invalid Time: Must be in the format xx:xx";
+            }
+            else
+            {
+                partySize = Int32.Parse(partySizeString);
+
+                if (partySize > 0 && partySize < 100)
+                {
+                    RsvtnTextBlockInvalidMessage.Visibility = System.Windows.Visibility.Hidden;
+
+                    Reservation newReservation = new Reservation(name, phoneNumber, expectedTime, "10:00", partySize);
+                    reservationController.createReservation(newReservation);
+                    reservationTimeSlots[reservationController.timeToIndex(expectedTime)].numSeats -= 4 * (int)Math.Ceiling((double)partySize / 4.0);
+                    reservationTimeSlots[reservationController.timeToIndex(expectedTime)].button.Content = reservationTimeSlots[reservationController.timeToIndex(expectedTime)].numSeats.ToString();
+                    RsvtnNewGrid.Visibility = System.Windows.Visibility.Hidden;
+                    ReservationScrollViewer.SetValue(Grid.RowProperty, 1);
+
+                    RsvtnTextBoxNewName.Text = "";
+                    RsvtnTextBoxNewNumber.Text = "";
+                    RsvtnTextBoxNewTime.Text = "";
+                    RsvtnTextBoxNewSize.Text = "";
+                }
+                else
+                {
+                    RsvtnTextBlockInvalidMessage.Visibility = System.Windows.Visibility.Visible;
+                    RsvtnTextBlockInvalidMessage.Text = "Invalid Party Size: Must be in range 1-99";
+                }
+            }
         }
 
+        private bool phoneNumberIsValid(string phoneString)
+        {
+            bool isValid = true;
+
+            if(phoneString.Length != 12)
+            {
+                isValid = false;
+            }
+            else if(phoneString.ToCharArray()[3] != '-')
+            {
+                isValid = false;
+            }
+            else if (phoneString.ToCharArray()[7] != '-')
+            {
+                isValid = false;
+            }
+            else
+            {
+                try
+                {
+                    Int32.Parse(phoneString.Substring(0, 3));
+                    Int32.Parse(phoneString.Substring(4, 3));
+                    Int32.Parse(phoneString.Substring(8, 4));
+
+                }
+                catch (FormatException)
+                {
+                    isValid = false;
+                }
+            }
+
+            return isValid;
+
+        }
+
+        private bool partySizeStringIsValid(string sizeString)
+        {
+            bool isValid = true;
+
+            try
+            {
+                Int32.Parse(sizeString);
+            }
+            catch(FormatException)
+            {
+                isValid = false;
+            }
+
+            return isValid;
+        }
+
+        private bool timeStringIsValid(string timeString)
+        {
+            bool isValid = true;
+
+            if(timeString.Length != 5 && timeString.Length != 4)
+            {
+                isValid = false;
+            }
+            else if(timeString.ToCharArray()[timeString.Length - 3] != ':')
+            {
+                isValid = false;
+            }
+            else
+            {
+                try
+                {
+                    Int32.Parse(timeString.Substring(timeString.Length - 2, 2));
+
+                    if(timeString.Length == 5)
+                    {
+                        Int32.Parse(timeString.Substring(0, 2));
+                    }
+                    else
+                    {
+                        Int32.Parse(timeString.Substring(0, 1));
+                    }
+                }
+                catch(FormatException)
+                {
+                    isValid = false;
+                }
+            }
+
+            return isValid;
+        }
         public void RsvtnBtnNewClick (object sender, RoutedEventArgs e)
         {
             RsvtnNewGrid.Visibility = System.Windows.Visibility.Visible;
+            ReservationScrollViewer.SetValue(Grid.RowProperty, 2);
+        }
+        public void RsvtnBtnCancelClick(object sender, RoutedEventArgs e)
+        {
+            RsvtnTextBlockInvalidMessage.Visibility = System.Windows.Visibility.Hidden;
+            RsvtnNewGrid.Visibility = System.Windows.Visibility.Hidden;
+            ReservationScrollViewer.SetValue(Grid.RowProperty, 1);
+
+            RsvtnTextBoxNewName.Text = "";
+            RsvtnTextBoxNewNumber.Text = "";
+            RsvtnTextBoxNewTime.Text = "";
+            RsvtnTextBoxNewSize.Text = "";
         }
 
         public void RsvtnBtnTime1Clk(object sender, RoutedEventArgs e)
         {
             ReservationGrid.ItemsSource = reservationController.getReservationsAtTime(0);
+            RsvtnTimeIndicator.SetValue(Grid.ColumnProperty, 0);
         }
 
         public void RsvtnBtnTime2Clk(object sender, RoutedEventArgs e)
         {
             ReservationGrid.ItemsSource = reservationController.getReservationsAtTime(1);
+            RsvtnTimeIndicator.SetValue(Grid.ColumnProperty, 2);
         }
 
         public void RsvtnBtnTime3Clk(object sender, RoutedEventArgs e)
         {
             ReservationGrid.ItemsSource = reservationController.getReservationsAtTime(2);
+            RsvtnTimeIndicator.SetValue(Grid.ColumnProperty, 4);
         }
 
         public void RsvtnBtnTime4Clk(object sender, RoutedEventArgs e)
         {
             ReservationGrid.ItemsSource = reservationController.getReservationsAtTime(3);
+            RsvtnTimeIndicator.SetValue(Grid.ColumnProperty, 6);
         }
 
         public void RsvtnBtnTime5Clk(object sender, RoutedEventArgs e)
         {
             ReservationGrid.ItemsSource = reservationController.getReservationsAtTime(4);
+            RsvtnTimeIndicator.SetValue(Grid.ColumnProperty, 8);
         }
 
         public void RsvtnBtnTime6Clk(object sender, RoutedEventArgs e)
         {
             ReservationGrid.ItemsSource = reservationController.getReservationsAtTime(5);
+            RsvtnTimeIndicator.SetValue(Grid.ColumnProperty, 10);
         }
         public void RsvtnBtnTime7Clk(object sender, RoutedEventArgs e)
         {
             ReservationGrid.ItemsSource = reservationController.getReservationsAtTime(6);
+            RsvtnTimeIndicator.SetValue(Grid.ColumnProperty, 13);
         }
     }
 }
